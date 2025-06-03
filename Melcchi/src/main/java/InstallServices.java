c0318e43722a957b7fd9d2bded75ad12ef7e4a18
Student <mkomi@appdev.yearup.org>
2025-06-03 08:27:57 -0500

    nothing

--- Melcchi/src/main/java/InstallServices.java
+++ Melcchi/src/main/java/InstallServices.java
@@ -1,2 +1,27 @@
 public class InstallServices {
+    DivinesWigs wigs;
+    DivinesLashes lashes;
+    DivinesExtensions extensions;
+
+
+    public InstallServices(DivinesExtensions extensions, DivinesLashes lashes, DivinesWigs wigs) {
+        this.extensions = extensions;
+        this.lashes = lashes;
+        this.wigs = wigs;
+    }
+
+    public DivinesExtensions getExtensions() {
+        return extensions;
+    }
+
+    public DivinesLashes getLashes() {
+        return lashes;
+    }
+
+    public DivinesWigs getWigs() {
+        return wigs;
+    }
+    public double serviceHairInstalls(DivinesWigs wigs,DivinesExtensions extensions) {
+
+    }
 }
